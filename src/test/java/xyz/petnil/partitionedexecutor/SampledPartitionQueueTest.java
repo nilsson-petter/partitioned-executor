84ea909e6e10b58dc2298980df0441b5118983d2
Petter Nilsson <petter.nil@outlook.com>
2024-10-13 22:40:31 +0200

    Updated test

--- src/test/java/xyz/petnil/partitionedexecutor/SampledPartitionQueueTest.java
+++ src/test/java/xyz/petnil/partitionedexecutor/SampledPartitionQueueTest.java
@@ -16,6 +16,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class SampledPartitionQueueTest {
+    private static final String PARTITION_KEY = "AAPL";
     private SampledPartitionQueue sampledPartitionQueue;
     private SamplingFunction samplingFunction;
     private PartitionedRunnable mockTask;
@@ -23,34 +24,26 @@ class SampledPartitionQueueTest {
     @BeforeEach
     void setUp() {
         samplingFunction = mock(SamplingFunction.class);
-        sampledPartitionQueue = new SampledPartitionQueue(samplingFunction);
+        when(samplingFunction.getSamplingTime(any())).thenReturn(Duration.ofMillis(50));
         mockTask = mock(PartitionedRunnable.class);
+        when(mockTask.getPartitionKey()).thenReturn(PARTITION_KEY);
+        sampledPartitionQueue = new SampledPartitionQueue(samplingFunction);
     }
 
     @Test
     void enqueue_shouldAddTask_whenNewPartitionKey() {
-        // Arrange
-        Object partitionKey = "key1";
-        when(mockTask.getPartitionKey()).thenReturn(partitionKey);
-        when(samplingFunction.getSamplingTime(partitionKey)).thenReturn(Duration.ofMillis(100));
-
-        // Act
         boolean result = sampledPartitionQueue.enqueue(mockTask);
-
-        // Assert
         assertThat(result).isTrue();
         assertThat(sampledPartitionQueue.getQueueSize()).isEqualTo(1);
     }
 
     @Test
-    void enqueue_shouldReplaceExistingTask_whenSamePartitionKey() throws InterruptedException {
-        // Arrange
-        Object partitionKey = "key1";
+    void enqueue_taskIsReplacedAndDropped() throws InterruptedException {
+        String partitionKey = "TSLA";
         PartitionedRunnable firstTask = mock(PartitionedRunnable.class);
         PartitionedRunnable secondTask = mock(PartitionedRunnable.class);
         when(firstTask.getPartitionKey()).thenReturn(partitionKey);
         when(secondTask.getPartitionKey()).thenReturn(partitionKey);
-        when(samplingFunction.getSamplingTime(partitionKey)).thenReturn(Duration.ofMillis(100));
         PartitionQueue.OnDroppedCallback onDroppedCallback = mock(PartitionQueue.OnDroppedCallback.class);
         sampledPartitionQueue.setOnDroppedCallback(onDroppedCallback);
 
@@ -65,48 +58,37 @@ class SampledPartitionQueueTest {
         PartitionedRunnable firstInQueue = sampledPartitionQueue.getNextTask(Duration.ofSeconds(1));
         assertEquals(secondTask, firstInQueue);
 
-        // Verify that the previous task was dropped
         verify(onDroppedCallback, times(1)).onDropped(firstTask);
     }
 
     @Test
     void getNextTask_shouldReturnTask_whenAvailable() throws InterruptedException {
-        // Arrange
-        Object partitionKey = "key1";
-        when(mockTask.getPartitionKey()).thenReturn(partitionKey);
-        when(samplingFunction.getSamplingTime(partitionKey)).thenReturn(Duration.ofMillis(100));
+        when(samplingFunction.getSamplingTime(any())).thenReturn(Duration.ofMillis(20));
         sampledPartitionQueue.enqueue(mockTask);
-
-        // Act
-        PartitionedRunnable retrievedTask = sampledPartitionQueue.getNextTask(Duration.ofMillis(150));
-
-        // Assert
+        PartitionedRunnable retrievedTask = sampledPartitionQueue.getNextTask(Duration.ofMillis(40));
         assertThat(retrievedTask).isEqualTo(mockTask);
         assertThat(sampledPartitionQueue.getQueueSize()).isEqualTo(0);
     }
 
     @Test
     void getNextTask_shouldReturnNull_whenNoTaskAvailable() throws InterruptedException {
-        // Act
         PartitionedRunnable retrievedTask = sampledPartitionQueue.getNextTask(Duration.ofMillis(100));
+        assertThat(retrievedTask).isNull();
+    }
 
+    @Test
+    void getNextTask_shouldReturnNull_whenNotAvailableYet() throws InterruptedException {
+        when(samplingFunction.getSamplingTime(any())).thenReturn(Duration.ofDays(1));
+        sampledPartitionQueue.enqueue(mockTask);
+        PartitionedRunnable retrievedTask = sampledPartitionQueue.getNextTask(Duration.ofMillis(1));
         // Assert
         assertThat(retrievedTask).isNull();
     }
 
     @Test
     void getQueueSize_shouldReturnCorrectSize() {
-        // Arrange
-        when(mockTask.getPartitionKey()).thenReturn("key1");
-        when(samplingFunction.getSamplingTime("key1")).thenReturn(Duration.ofMillis(100));
-
         sampledPartitionQueue.enqueue(mockTask);
-
-        // Act
-        int size = sampledPartitionQueue.getQueueSize();
-
-        // Assert
-        assertThat(size).isEqualTo(1);
+        assertThat(sampledPartitionQueue.getQueueSize()).isEqualTo(1);
     }
 
     @Test
