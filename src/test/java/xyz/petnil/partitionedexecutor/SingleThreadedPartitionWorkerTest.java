33ccb8b7e634c2bd25b8b406e11738a5af2eb508
Petter Nilsson <petter.nil@outlook.com>
2024-11-11 21:38:41 +0100

    Silence compiler warning

--- src/test/java/xyz/petnil/partitionedexecutor/SingleThreadedPartitionWorkerTest.java
+++ src/test/java/xyz/petnil/partitionedexecutor/SingleThreadedPartitionWorkerTest.java
@@ -76,7 +76,7 @@ class SingleThreadedPartitionWorkerTest {
 
     @Test
     void testPollAndProcessSuccess() throws InterruptedException {
-        when(mockQueue.getNextTask(any())).thenReturn(mockTask, null);
+        when(mockQueue.getNextTask(any())).thenReturn(mockTask, (PartitionedRunnable) null);
 
         worker.start();
 
@@ -115,7 +115,7 @@ class SingleThreadedPartitionWorkerTest {
     @Test
     void testCallbackOnError() throws InterruptedException {
         doThrow(new RuntimeException("Task failed")).when(mockTask).run();
-        when(mockQueue.getNextTask(any())).thenReturn(mockTask, null);
+        when(mockQueue.getNextTask(any())).thenReturn(mockTask, (PartitionedRunnable) null);
         worker.start();
         verify(mockCallback, timeout(1000)).onError(eq(mockTask), any(RuntimeException.class));
     }
